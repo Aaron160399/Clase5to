0ed12bfdd284509374da61adb5aeb99c43107e83
Aaron160399 <aaronstone68@hotmail.com>
2018-08-26 01:02:42 -0500

    Inicialización de proyectos.
    Se han iniciado todos los proyectos. Ahora ya está la primera actividad por realizar.

---
+++ src/Abraham/Inicializacion.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abraham;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Inicializacion {
+    public static void main(String[] args) {
+        /*Por medio de este mensaje se han inicializado los proyectos de NetBeans
+        con los cuales estaremos trabajando las actividades en clase y posiblemente
+        sus proyectos de soluciones empresariales.
+        Como primera instrucción deberán editar el método print de esta clase
+        de "Hola mundo" a "Su Nombre Completo - Enterado"
+        Ejemplo:
+        Aarón Lagunas Ramos - Enterado
+        */
+        System.out.println("Hola mundo");
+    }
+}
